476849931817befbf386f3329293777e49958b51
WangGuanru <frycrompton@gmail.com>
2023-01-01 22:47:41 +0800

    q

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -7,7 +7,7 @@ public class Collatz {
     public static int nextNumber(int n) {
         if (n%2==0) {
             return n/2;
-        } else if (n%2==1) {
+        } else{
             return 3 * n + 1;
         }
     }
