b601c534cf6638d040bd36e7c1a7c4545a699d19
WangGuanru <frycrompton@gmail.com>
2023-01-27 17:59:47 +0800

    lab7

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -3,59 +3,54 @@ package bstmap;
 import java.util.Iterator;
 import java.util.Set;
 
-public class BSTMap<K extends Comparable<K>,V> implements Map61B<K,V>{
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     private BSTNode root;
     private int size;
 
-    private class BSTNode{
-        K key;
-        V value;
-        BSTNode left;
-        BSTNode right;
-        BSTNode(K key,V value){
-            this.key=key;
-            this.value=value;
-        }
-    }
     @Override
     public void clear() {
-        root=null;
-        size=0;
+        root = null;
+        size = 0;
     }
 
     @Override
     public boolean containsKey(K key) {
-        return containsKey(root,key);
+        return containsKey(root, key);
     }
-    private boolean containsKey(BSTNode bstNode,K key){
-        if(bstNode==null)
+
+    private boolean containsKey(BSTNode bstNode, K key) {
+        if (bstNode == null) {
             return false;
+        }
         int temp = key.compareTo(bstNode.key);
-        if(temp>0)
-            return containsKey(bstNode.right,key);
-        else if(temp<0)
-            return containsKey(bstNode.left,key);
-        else
+        if (temp > 0) {
+            return containsKey(bstNode.right, key);
+        } else if (temp < 0) {
+            return containsKey(bstNode.left, key);
+        } else {
             return true;
+        }
     }
 
     @Override
     public V get(K key) {
-        return get(root,key);
-    }
-    private V get(BSTNode node,K key){
-        if(node==null)
-            return null;
-        int temp = key.compareTo(node.key);
-        if(temp>0)
-            return get(node.right,key);
-        else if(temp<0)
-            return get(node.left,key);
-        else
-            return node.value;
+        return get(root, key);
     }
 
+    private V get(BSTNode node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int temp = key.compareTo(node.key);
+        if (temp > 0) {
+            return get(node.right, key);
+        } else if (temp < 0) {
+            return get(node.left, key);
+        } else {
+            return node.value;
+        }
+    }
 
     @Override
     public int size() {
@@ -64,29 +59,30 @@ public class BSTMap<K extends Comparable<K>,V> implements Map61B<K,V>{
 
     @Override
     public void put(K key, V value) {
-        root = put(root,key,value);
+        root = put(root, key, value);
     }
 
-    private BSTNode put(BSTNode node,K key,V value){
-        if(node==null){
+    private BSTNode put(BSTNode node, K key, V value) {
+        if (node == null) {
             size++;
-            return new BSTNode(key,value);
+            return new BSTNode(key, value);
         }
         int temp = key.compareTo(node.key);
-        if(temp>0)
-            node.right=put(node.right,key,value);
-        else if(temp<0)
-            node.left=put(node.left,key,value);
+        if (temp > 0) {
+            node.right = put(node.right, key, value);
+        } else if (temp < 0) {
+            node.left = put(node.left, key, value);
+        }
         return node;
     }
 
     @Override
-    public Set keySet() {
+    public Set<K> keySet() {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public V remove(K key){
+    public V remove(K key) {
         throw new UnsupportedOperationException();
     }
 
@@ -95,9 +91,24 @@ public class BSTMap<K extends Comparable<K>,V> implements Map61B<K,V>{
         throw new UnsupportedOperationException();
     }
 
-
     @Override
-    public Iterator iterator() {
+    public Iterator<K> iterator() {
         throw new UnsupportedOperationException();
+    }
+
+    public void printInOrder() {
+
+    }
+
+    private class BSTNode {
+        K key;
+        V value;
+        BSTNode left;
+        BSTNode right;
+
+        BSTNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
